11d77eece9e9f4458161175ef74d50bc0e9f5538
valenzarria <111452384+valenzarria@users.noreply.github.com>
2022-08-17 14:06:37 -0300

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+#include <util/delay.h>
+
+#define bot1 ((PIND >> 4) & 0x01)
+#define bot2 ((PIND >> 5) & 0x01)
+#define bot3 ((PIND >> 6) & 0x01)
+#define bot4 ((PIND >> 7) & 0x01) 
+
+#define prender(PORT, PIN) (PORT |= 1 << PIN)
+#define apagar(PORT, PIN) (PORT &= ~(1 << PIN))
+
+int main(){
+  DDRD &= 0x0F;
+  DDRB |= 0x0F;
+
+  while(1){
+    if (bot1==0)
+    {
+      prender(PORTB,0);
+    }
+
+    if (bot2==0)
+    {
+      prender(PORTB,1);
+    }
+
+    if (bot3==0)
+    {
+      prender(PORTB,2);
+    }
+
+    if (bot4==0)
+    {
+      prender(PORTB,3);
+    }
+  }
+}
